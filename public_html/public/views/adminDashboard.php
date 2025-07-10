8d06af3a3b8f0935b90993f785ae17c95ad82c2b
eltamales <jesushinojo9@gmail.com>
2025-07-10 13:37:32 -0600

    Cambios en el diseno del header y subida de nuevos archivos de css

--- public_html/public/views/adminDashboard.php
+++ public_html/public/views/adminDashboard.php
@@ -29,6 +29,14 @@ $conn->query("SET time_zone='-06:00'");
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp" crossorigin="anonymous">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css">
   <link rel="stylesheet" href="../css/pages/baseTsoft.css" />
+
+  <!-- CSS Personalizado -->
+  <link rel="stylesheet" href="../css/base.css">
+  <link rel="stylesheet" href="../css/components/sidebar.css">
+  <link rel="stylesheet" href="../css/components/cards.css">
+  <link rel="stylesheet" href="../css/components/tables.css">
+  <link rel="stylesheet" href="../css/layout.css">
+  <link rel="stylesheet" href="../css/pages/adminDashboard.css">
 </head>
 
 <?php
