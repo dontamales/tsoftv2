71a6a2f9422e32e3ea7271630c0595fd9dfd4dad
eltamales <jesushinojo9@gmail.com>
2025-08-04 17:05:59 -0600

    TS-9 Busqueda de usuarios por numero de control

--- public_html/public/js/tablaUsuario.js
+++ public_html/public/js/tablaUsuario.js
@@ -13,6 +13,10 @@ $(document).ready(function() {
       // Llena la tabla con los datos obtenidos
       $.each(data, function(index, row) {
         var newRow = $("<tr>");
+        // Agrega atributo oculto si es sustentante JH20250804
+        if (row.Fk_Roles_Usuario === 1 && row.Num_Control) {
+          newRow.attr("data-control", row.Num_Control.toLowerCase());
+        }
         var checkbox = $("<input>").attr("type", "checkbox").addClass("selectUser");
         if (row.FK_Estatus_Egresado <= 3) {
           checkbox.attr("disabled", false);
@@ -47,9 +51,16 @@ $(document).ready(function() {
         newRow.append($("<td>").text(rol));
         newRow.append($("<td>").text(row.Nombres_Usuario));
         newRow.append($("<td>").text(row.Apellidos_Usuario));
+        // Nueva columna para el número de control
+        // Solo se muestra si el usuario es un sustentante (Fk_Roles_Usuario === 1)
+        // y tiene un número de control definido JH20250804
+        if (row.Fk_Roles_Usuario === 1 && row.Num_Control) {
+          newRow.append($("<td>").text(row.Num_Control));
+        } else {
+          newRow.append($("<td>").text("N/A"));
+        }
         newRow.append($("<td>").text(row.Correo_Usuario));
-        newRow.append($("<td>").text(row.Fecha_Usuario));
-        
+        newRow.append($("<td>").text(row.Fecha_Usuario));  
         tbody.append(newRow);
       });
     },
@@ -59,17 +70,55 @@ $(document).ready(function() {
   });
 });
 
+// Funcion de Busqueda anterior
+// $("#buscarBtn").click(function() {
+//   var searchTerm = $("#searchUsuario").val().toLowerCase();
+//   $("#tabla-usuario-lista tbody tr").each(function() {
+//       var lineStr = $(this).text().toLowerCase();
+//       if (lineStr.indexOf(searchTerm) === -1) {
+//           $(this).hide();
+//       } else {
+//           $(this).show();
+//       }
+//   });
+// });
+
+// Búsqueda por texto y número de control, celda por celda JH20250804
 $("#buscarBtn").click(function() {
   var searchTerm = $("#searchUsuario").val().toLowerCase();
+
   $("#tabla-usuario-lista tbody tr").each(function() {
-      var lineStr = $(this).text().toLowerCase();
-      if (lineStr.indexOf(searchTerm) === -1) {
-          $(this).hide();
-      } else {
-          $(this).show();
+    var row = $(this);
+    var celdas = row.find("td");
+    var coincide = false;
+
+    // Buscar en celdas visibles
+    celdas.each(function() {
+      var texto = $(this).text().toLowerCase();
+      if (texto.includes(searchTerm)) {
+        coincide = true;
+        return false; // salir del .each
       }
+    });
+
+    // Buscar también en el atributo data-control
+    var controlAttr = row.attr("data-control") || "";
+    if (controlAttr.includes(searchTerm)) {
+      coincide = true;
+    }
+
+    // Mostrar u ocultar
+    row.toggle(coincide);
   });
 });
+
+// Buscar también con tecla Enter JH20250804
+$("#searchUsuario").keypress(function(e) {
+  if (e.which === 13) {
+    $("#buscarBtn").click();
+  }
+});
+
 
 $("#borrarBtnUsuarios").click(function() {
   // Recolectar los IDs de los usuarios seleccionados
